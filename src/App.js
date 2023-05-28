3827d137c39b817bd3bf4703cc280ec8e276edee
RevanthKumar <revanani1801@gmail.com>
2023-05-28 21:33:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./components/Home";
 import "./style.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Home/>
     </div>
   );
 }
